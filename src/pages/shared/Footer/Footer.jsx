07cbcf6a3cb635f40c3248368b5930fd5a31d8a7
acchayon <adhikaryac81@gmail.com>
2023-05-01 23:01:13 +0600

    added reasonable footer

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
+
 
 const Footer = () => {
     return (
-        <div>
-            <h2 className='text-center'>This is footer</h2>
-        </div>
+        <Container style={{height: '30vh'}} className='bg-dark text-white text-center'>
+            <h2  className=' pt-4'>Thanks for visiting to our Best Chefs & Recipis</h2>
+            <p>Contacts Us</p>
+            <div className='d-flex gap-4 fs-4 pb-2  justify-content-center'>
+                <FaFacebook></FaFacebook>
+                <FaYoutube></FaYoutube>
+                <FaInstagram></FaInstagram>
+            </div>
+            <p>© 2023 All Rights Reserved. Designed by AC Chayon</p>
+        </Container>
     );
 };
 
