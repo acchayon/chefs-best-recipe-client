cdaa4d3f8880ec3e2a22f445d8f5ffa16f1f508e
acchayon <adhikaryac81@gmail.com>
2023-05-01 22:20:53 +0600

    added header navbar

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            <h2>header</h2>
-        </div>
+        <Container>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Navbar.Brand href="#home"><h1 className='ps-2'>Best <span className='text-primary'>Recipe</span></h1></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link className='text-success' href="#features">Home</Nav.Link>
+                        <Nav.Link className='text-success' href="#pricing">Blog</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">Profile</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            <Button variant="primary">Login</Button>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </Container>
     );
 };
 
