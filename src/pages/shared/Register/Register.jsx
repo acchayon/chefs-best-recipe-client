e8bf9c9d1b783cbcd5bf7a26af05393067c70fd1
acchayon <adhikaryac81@gmail.com>
2023-05-02 23:55:43 +0600

    added register page

--- src/pages/shared/Register/Register.jsx
+++ src/pages/shared/Register/Register.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register</h2>
-        </div>
+        <Container>
+            <Form className='w-50 mx-auto'>
+                <h2 className='text-center '>Please Registe !!!</h2>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter your name" required />
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control type="text" name='photo' placeholder="Photo" required />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+                <p>Already have an account? Go to <Link to='/login'>login</Link> </p>
+            </Form>
+        </Container>
     );
 };
 
